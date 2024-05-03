325a38990b69be4c634faf15d6e4ebd1b858824a
liushuaibiao <misteryliu@outlook.com>
2024-05-03 19:34:51 +0800

    fix

--- src/main/java/com/macro/mall/tiny/modules/map/service/impl/AroundMapServiceImpl.java
+++ src/main/java/com/macro/mall/tiny/modules/map/service/impl/AroundMapServiceImpl.java
@@ -91,19 +91,26 @@ public class AroundMapServiceImpl implements AroundMapService {
         File file = createFile(originalPath);
         String ossPath;
         String searchParams;
+        String tag ="";
+        String industry_type = "";
         if (BusTypeEnum.FOOD.equals(busTypeEnum) && keys.contains("all")) {
             searchParams = "美食,附近美食,快餐";
+//            tag = "中餐厅,外国餐厅,小吃快餐店,咖啡厅";
+            industry_type = "cater";
         } else if (BusTypeEnum.SHOP.equals(busTypeEnum) && keys.contains("all")) {
             searchParams = "购物中心,商场,超市,便利店,小卖部,菜市场";
+//            tag = "购物中心,百货商场,超市,便利店,家居建材,家电数码,商铺,市场,其他";
+            industry_type = "life";
         } else {
             // 组装搜索参数
             searchParams = String.join(",", keys);
         }
         // 搜索结果放入json
         JSONArray jsonArray = new JSONArray();
-
+//        tag = "".equals(tag) ? "" : "&tag="+tag;
+        industry_type = "".equals(industry_type) ? "" : "&industry_type="+industry_type;
         try (FileWriter writer = new FileWriter(originalPath)) {
-            String firstUrl = searchUrl + searchParams + "&location=" + centerLocation + "&radius=5000&output=json&ak=d6KFBCpxKoA7Y1i3JCOpBMmsPq4lm7tI&page_num=1&page_size=20";
+            String firstUrl = searchUrl + searchParams + "&location=" + centerLocation + "&radius=5000&output=json&ak=d6KFBCpxKoA7Y1i3JCOpBMmsPq4lm7tI&page_num=1&page_size=20&scope=2" + tag + industry_type+"&filter=sort_name:distance|sort_rule:1";
             log.info("firstUrl:{}", firstUrl);
             String firstResp = HttpUtil.createGet(firstUrl).execute().body();
             CircleLocationDTO firstLocation = JSONObject.parseObject(firstResp, CircleLocationDTO.class);
